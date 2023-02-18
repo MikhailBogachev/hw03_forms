a67172575da7eafa38ff99dc2dfd090f65a18118
Михаил Богачев <bogachevmikhailirkutsk@gmail.com>
2023-02-18 16:21:33 +0800

    Add test views

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,12 +1,15 @@
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from django import forms
+
+import time
 
 from posts.models import Post, Group
 
 User = get_user_model()
 
-class StaticURLTests(TestCase):
+class StaticViewTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -19,17 +22,17 @@ class StaticURLTests(TestCase):
             description='Описание группы'
         )
         cls.post = Post.objects.create(
-            text='Текст поста',
+            text=f'Текст поста',
             author=cls.user,
             group=cls.group
-        )
+        )      
 
     def setUp(self):
         # Создаем неавторизованный клиент
         self.guest_client = Client()
         # Создаем авторизованый клиент
         self.authorized_client = Client()
-        self.authorized_client.force_login(StaticURLTests.user)
+        self.authorized_client.force_login(StaticViewTests.user)
 
     # Проверяем используемые шаблоны
     def test_pages_uses_correct_template(self):
@@ -94,6 +97,167 @@ class StaticURLTests(TestCase):
         self.assertEqual(task_post_group, 'Название группы')
         self.assertEqual(task_group_title, 'Название группы')
         self.assertEqual(task_group_slug, 'test-slug')
-        self.assertEqual(task_group_description, 'Описание группы')       
+        self.assertEqual(task_group_description, 'Описание группы')
 
+    def test_profile_page_correct_context(self):
+        """Шаблон profile сформирован с правильным контекстом"""
+        response = self.authorized_client.get(
+            reverse('posts:profile', kwargs={'username': 'User'})
+        )
+        first_object = response.context['page_obj'][0]
+        author_object = response.context['author']
+        post_count_object = response.context['post_count']
+        task_post_text = first_object.text
+        task_post_author = first_object.author.username
+        task_post_group = first_object.group.title
+        task_author_username = author_object.username
+        task_post_count = post_count_object
+
+        self.assertEqual(task_post_text, 'Текст поста')
+        self.assertEqual(task_post_author, 'User')
+        self.assertEqual(task_post_group, 'Название группы')
+        self.assertEqual(task_author_username, 'User')
+        self.assertEqual(task_post_count, 1)
+
+    def test_post_detail_page_correct_context(self):
+        """Шаблон post_detail сформирован с правильным контекстом"""
+        response = self.authorized_client.get(
+            reverse('posts:post_detail', kwargs={'post_id': 1})
+        )
+        first_object = response.context['post']
+        print(first_object)
+        post_count_object = response.context['post_count']
+        task_post_text = first_object.text
+        task_post_author = first_object.author.username
+        task_post_group = first_object.group.title
+        task_post_count = post_count_object
+
+        self.assertEqual(task_post_text, 'Текст поста')
+        self.assertEqual(task_post_author, 'User')
+        self.assertEqual(task_post_group, 'Название группы')
+        self.assertEqual(task_post_count, 1)
+
+    def test_post_create_page_correct_context(self):
+        """Шаблон post_create сформирован с правильным контекстом"""
+        response = self.authorized_client.get(
+            reverse('posts:post_create')
+        )
+
+        form_fields = {          
+            'text': forms.fields.CharField,
+            'group': forms.fields.ChoiceField,
+        }  
+
+        for value, expected in form_fields.items():
+            with self.subTest(value=value):
+                form_field = response.context.get('form').fields.get(value)
+                self.assertIsInstance(form_field, expected)
+        
+        first_group = response.context['group'][0]
+        task_group = first_group.title
+        self.assertEqual(task_group, 'Название группы')
+
+    def test_post_edit_page_correct_context(self):
+        """Шаблон post_edit сформирован с правильным контекстом"""
+        response = self.authorized_client.get(
+            reverse('posts:post_edit', kwargs={'post_id': 1})
+        )
+
+        form_fields = {          
+            'text': forms.fields.CharField,
+            'group': forms.fields.ChoiceField,
+        }  
+        for value, expected in form_fields.items():
+            with self.subTest(value=value):
+                form_field = response.context.get('form').fields.get(value)
+                self.assertIsInstance(form_field, expected)
+
+        first_group = response.context['group'][0]
+        post_obj = response.context['post']
+        key_is_edit = response.context['is_edit']
+        task_group = first_group.title
+        task_post_text = post_obj.text
+        task_post_author = post_obj.author.username
+        #task_post_group = post_obj.group.title
+        task_is_edit = key_is_edit
+
+        self.assertEqual(task_group, 'Название группы')
+        self.assertEqual(task_post_text, 'Текст поста')
+        self.assertEqual(task_post_author, 'User')
+        #self.assertEqual(task_post_group, 'Название группы')
+        self.assertEqual(task_is_edit, True)
+
+
+class PaginatorViewTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create(
+            username='User'
+        )
+        cls.group = Group.objects.create(
+            title='Название группы',
+            slug='test-slug',
+            description='Описание группы'
+        )
+        for num_post in range(1, 16):
+            cls.post = Post.objects.create(
+                text=f'Текст поста {num_post}',
+                author=cls.user,
+                group=cls.group
+            ) 
     
+    def setUp(self):
+        # Создаем неавторизованный клиент
+        self.guest_client = Client()
+        # Создаем авторизованый клиент
+        self.authorized_client = Client()
+        self.authorized_client.force_login(PaginatorViewTests.user)
+
+    def test_index_first_page(self):
+        """Шаблон index выводит нужное кол-во постов на первую страницу"""
+        response = self.authorized_client.get(
+            reverse('posts:index')
+        )
+        self.assertEqual(len(response.context['page_obj']), 10)
+    
+    def test_index_second_page(self):
+        """Шаблон index выводит нужное кол-во постов на вторую страницу"""
+        response = self.authorized_client.get(
+            reverse('posts:index') + '?page=2'
+        )
+        self.assertEqual(len(response.context['page_obj']), 5)
+    
+    def test_group_post_first_page(self):
+        """Шаблон group_post выводит нужное кол-во постов на первую страницу"""
+        response = self.authorized_client.get(
+            reverse('posts:group_list', kwargs={'slug': 'test-slug'})
+        )
+        self.assertEqual(len(response.context['page_obj']), 10)
+    
+    def test_group_post_second_page(self):
+        """Шаблон group_post выводит нужное кол-во постов на вторую страницу"""
+        response = self.authorized_client.get(
+            reverse(
+                'posts:group_list', kwargs={'slug': 'test-slug'}
+            )
+            + '?page=2'
+        )
+        self.assertEqual(len(response.context['page_obj']), 5)
+    
+    def test_profile_first_page(self):
+        """Шаблон profile выводит нужное кол-во постов на первую страницу"""
+        response = self.authorized_client.get(
+            reverse('posts:profile', kwargs={'username': 'User'})
+        )
+        self.assertEqual(len(response.context['page_obj']), 10)
+    
+    def test_profile_second_page(self):
+        """Шаблон profile выводит нужное кол-во постов на вторую страницу"""
+        response = self.authorized_client.get(
+            reverse(
+                'posts:profile', kwargs={'username': 'User'}
+            )
+            + '?page=2'
+        )
+        self.assertEqual(len(response.context['page_obj']), 5)
